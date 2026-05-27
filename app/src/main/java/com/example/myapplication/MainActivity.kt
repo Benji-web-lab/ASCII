ed7e132225c3653eb7f62c8b72d642fcc89e0ac7
Benji-web-lab <benginobenjamim007@gmail.com>
2026-05-27 11:01:09 +0200

    ASCII-ART

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.Display
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.TXTDISPLAY)
+
+        // Variables
+        var row = 1
+        val maxRows = 7
+        var asciiArt = ""
+
+        // While loop to build the pattern
+        while (row <= maxRows) {
+            var col = 1
+            var line = ""
+
+            while (col <= row) {
+                line += "* "
+                col++
+            }
+
+            asciiArt += line.trim() + "\n"
+            row++
+        }
+
+        textView.text = asciiArt
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
